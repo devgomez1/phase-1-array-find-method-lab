33abb2c81a3748c811e68d99abe5a1e11def61c1
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-06 09:39:10 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(array) {
+  const win = array.find((record) => record.result === "W");
+  if (win) {
+    console.log(`The Denver Broncos won the superbowl in: ${win.year}.`);
+    return win.year;
+  }
+  return undefined;
+}
+
+superbowlWin(record);
